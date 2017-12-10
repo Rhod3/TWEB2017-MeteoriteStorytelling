eb6f89eb68fd421c88392c3353bbda5ab193f9a5
Basile portable <basile.chatillon@heig-vd.ch>
2017-12-10 23:45:49 +0100

    mise en commentaire des print dans la console et doc

--- docs/vendor/js/meteoritePerYearGraph.js
+++ docs/vendor/js/meteoritePerYearGraph.js
@@ -1,13 +1,14 @@
 // set the dimensions and margins of the graph
-
 var margin = { top: 30, right: 50, bottom: 30, left: 50 },
   width2 = 1200 - margin.left - margin.right,
   height2 = 600 - margin.top - margin.bottom;
 
+// Function to sort numbers
 function sortNumber(a, b) {
   return a - b;
 }
 
+// Function to count the occurrences / frequency of array elements
 function count(arr) {
   var a = [], b = [], prev;
 
@@ -25,6 +26,7 @@ function count(arr) {
   return [a, b];
 }
 
+// parse the date in an utc format
 var iso = d3.time.format.utc("%Y-%m-%dT%H:%M:%S.%L");
 
 // get the data
@@ -32,11 +34,11 @@ d3.json("datasets/earthMeteoriteLandings.json", function (error, data) {
   if (error) return console.log("datasets/earthMeteoriteLandings.json" + error); //unknown error, check the console  
 
   var date = [];
-  var test = 0;
-  console.log("length of the data : " + data.length);
+  //console.log("length of the data : " + data.length);
+
   // format the data
   data.forEach(function (d) {
-    test++;
+    // There might have some comets without any year
     if (d.year != undefined) {
       d.year = iso.parse(d.year);
       d.year = d.year.getFullYear();
@@ -44,39 +46,33 @@ d3.json("datasets/earthMeteoriteLandings.json", function (error, data) {
     }
   });
 
-
+  // We sort the date
   date = date.sort(sortNumber);
-  console.log(date);
+  //console.log(date);
   var [a, b] = count(date);
   a = a.sort(sortNumber);
 
+  // set the parameters for the histogram
   var histogram = d3.layout.histogram()
-    .bins(a)(date);
+    .bins(a)(date); // We want each date to be a bin
 
-  console.log(histogram);
+  //console.log(histogram);
+  //console.log(a);
+  //console.log(b);
+  //console.log(width2);
+  //console.log(a.length);
+  //console.log([0, d3.max(histogram.map(function (d) { return d.length; }))]);
 
-
-  console.log(a);
-  console.log(b);
-  console.log(width2);
-  console.log(a.length);
-  console.log([0, d3.max(histogram.map(function (d) { return d.length; }))]);
-
-  // Scale the range of the data in the domains
-  //var x.domain(a);
-  // y.domain([0, d3.max(histogram.map(function (d) { return d.length; }))]);
-
-
-
+  
   var x = d3.scale.linear()
-    .domain([d3.min(date), d3.max(date)])
-    .range([0, width2 - 20]);
+    .domain([d3.min(date), d3.max(date)]) // set the ranges
+    .range([0, width2]); // Scale the range of the data in the domains
 
   var y = d3.scale.linear()
-    .domain([0, d3.max(histogram, function (i) { return i.length; })])
-    .range([height2, 0]);
+    .domain([0, d3.max(histogram, function (i) { return i.length; })]) // set the ranges
+    .range([height2, 0]); // Scale the range of the data in the domains
 
-  // append the svg object to the body of the page
+  // append the svg object to the meteoritePerYear id of the page
   // append a 'group' element to 'svg'
   // moves the 'group' element to the top left margin
   var svg2 = d3.select("#meteoritePerYear").append("svg")
@@ -85,18 +81,19 @@ d3.json("datasets/earthMeteoriteLandings.json", function (error, data) {
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");;
 
-  // append the rectangles for the bar chart
+  // Creation of the bar for the histogram
   var bars2 = svg2.selectAll(".bar")
     .data(histogram)
     .enter()
     .append("g");
 
+  // append the rectangles for the bar chart
   bars2.append("rect")
     .attr("x", function (d) { return x(d.x); })
     .attr("y", function (d) { return y(d.y); })
-    .attr("width", 3)
+    .attr("width", width2/a.length)
     .attr("height", function (d) { return height2 - y(d.y); })
-    .attr("fill", "darkblue")
+    .attr("fill", "darkblue") // Nice color
     .on("mouseover", (d) => {
       div.transition()
         .duration(20)
@@ -113,19 +110,24 @@ d3.json("datasets/earthMeteoriteLandings.json", function (error, data) {
         .style("top", 0);
     });
 
+  // Creation of the x axis
   var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom"); // the text is at the bottom
 
   var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+    .scale(y) // Creation of the x axis
+    .orient("left") // the text is at the bottom
+    .innerTickSize(-width2) // To show the lines
+    .outerTickSize(0);
 
+  // add the x Axis
   svg2.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0," + height2 + ")")
+    .attr("transform", "translate(0," + height2 + ")") // we have to translate it so he is not in the top of the graph
     .call(xAxis);
 
+  // add the y Axis
   svg2.append("g")
     .attr("class", "y axis")
     .call(yAxis);
